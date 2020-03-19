6f859ca4f11e19ed7fca0563a52d7a9769c64bd3
Vedaukr <kalipso121221@gmail.com>
2020-03-19 23:22:13 +0200

    Fixed loosely created gitignore.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImageConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
